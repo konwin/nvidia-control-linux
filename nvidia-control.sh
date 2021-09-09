fccfbea6aaf8ea8c780fd256c6b2b1fe8b118f7f
konwin <44242369+konwin@users.noreply.github.com>
2021-09-09 10:36:29 +0200

    Update nvidia-control.sh
    adapted to run with RTX3xxx cards and added functionality for multiple gpus (been running it with 10 gpus).

--- nvidia-control.sh
+++ nvidia-control.sh
@@ -32,7 +32,13 @@ LICENSE
 # To set a negative number, set it like this: GCLOCK=${GCLOCK:--30}
 
 # Which GPU to use. find with nvidia-smi -L
-GPUID=${GPUID:-0}
+# GPUID=${GPUID:-9}
+
+# How many GPUs to use.
+GPUQTY=${GPUQTY:-10}
+
+# Fanoffset. RTX30x0 fans decrease by 2, not by 1!
+FANOFFS=${FANOFFS:-2}
 
 # Set the GPU in its highest P-State. Any value will work. Leave empty to disable.
 # For example, setting this to true will force the GPU to its highest P-State.
@@ -40,20 +46,44 @@ POWERMIZER=${POWERMIZER:-}
 
 # Set the power limit (percentage, can be higher than 100) of the GPU. Leave empty to disable.
 # For example, if you set this to 50, and your GPU's max power draw limit is 200 watts,
-# this will limit the GPU to 100 watts.
-POWER=${POWER:-}
+# this will limit the GPU to 100 watts. 65
+#POWER=${POWER:-67}
+if [[ $POWER ]]; then
+	echo 'POWER from command line'; 
+else
+	POWER=(70 70 65 65 65 65 65 65 65 65)
+fi
 
 # Set the GPU clock speed offset in MHz (can be a negative number). Leave empty to disable.
 # For example, if your GPU is 1000MHz and you set this to 50, your GPU will be 1050MHz.
-GCLOCK=${GCLOCK:-}
+#GCLOCK=${GCLOCK:--500}
+if [[ $GCLOCK ]]; then
+	echo 'GCLOCK from command line'; 
+else
+	GCLOCK=(-500 -500 -500 -500 -500 -500 -500 -500 -500 -500)
+fi
+
+# LOCK the GPU clock speed in MHz (absolute value). Leave empty to disable.
+# For example, if you set this to 950, your GPU will be 950MHz. 825
+#LGCLOCK=${LGCLOCK:-850}
+if [[ $LGCLOCK ]]; then
+	echo 'LGCLOCK from command line'; 
+else
+	LGCLOCK=(850 850 850 850 850 850 850 850 850 850)
+fi
 
 # Set the memory clock speed offset in MHz (can be a negative number). Leave empty to disable.
-# For example, if your memory is 2000MHz and you set this to -100, your memory will be 1900MHz.
-MCLOCK=${MCLOCK:-}
+# For example, if your memory is 2000MHz and you set this to -100, your memory will be 1900MHz. 2450
+#MCLOCK=${MCLOCK:-2500}
+if [[ $MCLOCK ]]; then
+	echo 'MCLOCK from command line'; 
+else
+	MCLOCK=(2100 2200 2400 2350 2300 2350 2100 2400 2300 2350)
+fi
 
-# Which P-State to work on for the GPU / memory clock speed offset, 3 is the highest on modern Nvidia GPUs.
+# Which P-State to work on for the GPU / memory clock speed offset, 3 is the highest on modern Nvidia GPUs. (4 on RTX30x0)
 # Usually only the highest P-State can be changed. You can find the perf levels for your card with: nvidia-settings -q GPUPerfModes
-PSTATE=${PSTATE:-3}
+PSTATE=${PSTATE:-4}
 
 # How many seconds to wait before checking temps / setting fan speeds. Lower values mean higher CPU usage. Set it empty to disable fan control.
 INTERVAL=${INTERVAL:-5.0}
@@ -62,7 +92,7 @@ INTERVAL=${INTERVAL:-5.0}
 SHOWMAP=${SHOWMAP:-}
 
 # Show the current speed / temp when controlling the fan. Leave empty to disable.
-SHOWCURRENT=${SHOWCURRENT:-}
+SHOWCURRENT=${SHOWCURRENT:-1}
 
 # Set the LED brightness in percentage (assuming your card has LED's). Can be a number between 0 and 100. Leave empty to keep the default brightness.
 # For example, setting this to 0 will disable the LED's.
@@ -75,13 +105,13 @@ MINSPEED=0
 # What fan speed to set at what temperature, for example set the fan speed at 25% when GPU temp is 50 degrees.
 # All other values are calculated on the fly, pass the SHOWMAP=true environment variable to show the calculated values.
 TEMP[0]=50
-SPEED[0]=25
+SPEED[0]=30
 
 TEMP[1]=60
-SPEED[1]=50
+SPEED[1]=60
 
 TEMP[2]=70
-SPEED[2]=75
+SPEED[2]=80
 
 TEMP[3]=80
 SPEED[3]=100
@@ -120,101 +150,139 @@ fi
 trap cleanup SIGHUP SIGINT SIGQUIT SIGFPE SIGTERM
 function cleanup() {
     echo "Exiting, cleaning up."
-    if [[ $CHANGEDPM ]]; then
-        echo "Disabling persistance mode. (Requires root)"
-        sudo nvidia-smi --persistence-mode=0 --id="$GPUID" 1> /dev/null
-        nvidia-smi > /dev/null
-    fi
-    if [[ $CPDRAW ]]; then
-        echo "Reverting power limit to $CPDRAW. (Requires root)"
-        sudo nvidia-smi --id="$GPUID" -pl "$CPDRAW" 1> /dev/null
-    fi
-    if [[ $CHANGEDFS ]]; then
-        echo "Enabling automatic fan control."
-        nvidia-settings --assign [gpu:$GPUID]/GPUFanControlState=0 1> /dev/null
-    fi
-    if [[ $POWERMIZER ]]; then
-        echo "Setting automatic P-State control."
-        nvidia-settings --assign [gpu:$GPUID]/GPUPowerMizerMode=1 1> /dev/null
-    fi
-    if [[ -z $1 ]]; then
-        exit
-    fi
-    exit "$1"
+	for ((i=0; i<$GPUQTY; i++))
+	do
+		GPUID=$i
+		if [[ $CHANGEDPM ]]; then
+			echo "Disabling persistance mode on GPU$i. (Requires root)"
+			sudo nvidia-smi --persistence-mode=0 --id="$GPUID" 1> /dev/null
+			nvidia-smi > /dev/null
+		fi
+		if [[ ${CPDRAW[$i]} ]]; then
+			echo "Reverting power limit to ${CPDRAW[$i]} on GPU$i. (Requires root)"
+			sudo nvidia-smi --id="$GPUID" -pl "${CPDRAW[$i]}" 1> /dev/null
+		fi
+		if [[ $CHANGEDFS ]]; then
+			echo "Enabling automatic fan control on GPU$i."
+			nvidia-settings --assign [gpu:$GPUID]/GPUFanControlState=0 1> /dev/null
+		fi
+		if [[ $POWERMIZER ]]; then
+			echo "Setting automatic P-State control on GPU$i."
+			nvidia-settings --assign [gpu:$GPUID]/GPU															MizerMode=1 1> /dev/null
+		fi
+	done
+	if [[ -z $1 ]]; then
+		exit
+	fi
+	exit "$1"
 }
 
 if [[ $POWER ]]; then
     CHANGEDPM=1
-    echo "Enabling persistence mode for gpu $GPUID. (Requires root)"
-    sudo nvidia-smi --persistence-mode=1 --id="$GPUID" 1> /dev/null
-    PDRAW=$(nvidia-smi --id="$GPUID" -q -d POWER)
-    if [[ -z $PDRAW ]]; then
-        echo "Error probing Nvidia power limit!"
-        cleanup 1
-    fi
-    MIPDRAW=$(echo "$PDRAW" | grep "Min Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
-    MAPDRAW=$(echo "$PDRAW" | grep "Max Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
-    CPDRAW=$(echo "$PDRAW" | grep "^\s*Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
-    PDRAW=$(echo "$PDRAW" | grep "Default Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
-    if [[ -z $MIPDRAW ]] || [[ -z $MAPDRAW ]] || [[ -z $PDRAW ]] || [[ -z $CPDRAW ]]; then
-        echo "Error parsing power draw limits from Nvidia GPU!"
-        cleanup 1
-    elif [[ $POWER -lt 1 ]]; then
-        echo "Power limit is lower than 1%, setting to 1%"
-        POWER=1
-    fi
-    WPDRAW=$(($(($((PDRAW * 10)) * POWER)) / 1000))
-    if [[ $WPDRAW -gt $MAPDRAW ]]; then
-        echo "The Nvidia driver allows a maximum of $MAPDRAW watts for this GPU. Setting to maximum."
-        WPDRAW=$MAPDRAW
-    elif [[ $WPDRAW -lt $MIPDRAW ]]; then
-        echo "The Nvidia driver allows a minimum of $MIPDRAW watts for this GPU. Setting to minimum."
-        WPDRAW=$MIPDRAW
-    fi
-    echo "Setting power limit to ${POWER}% (${WPDRAW} watts). (Requires root)"
-    sudo nvidia-smi --id="$GPUID" -pl "$WPDRAW" 1> /dev/null
+	for ((i=0; i<$GPUQTY; i++))
+	do
+		GPUID=$i
+		echo "Enabling persistence mode for gpu $GPUID. (Requires root)"
+		sudo nvidia-smi --persistence-mode=1 --id="$GPUID" 1> /dev/null
+		PDRAW[$i]=$(nvidia-smi --id="$GPUID" -q -d POWER)
+		if [[ -z ${PDRAW[$i]} ]]; then
+			echo "Error probing Nvidia power limit!"
+			cleanup 1
+		fi
+		MIPDRAW=$(echo "${PDRAW[$i]}" | grep "Min Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
+		MAPDRAW=$(echo "${PDRAW[$i]}" | grep "Max Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
+		CPDRAW[$i]=$(echo "${PDRAW[$i]}" | grep "^\s*Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
+		PDRAW[$i]=$(echo "${PDRAW[$i]}" | grep "Default Power Limit" | cut -d: -f2 | grep -Po "^\s*\d+" | xargs)
+		if [[ -z $MIPDRAW ]] || [[ -z $MAPDRAW ]] || [[ -z ${PDRAW[$i]} ]] || [[ -z ${CPDRAW[$i]} ]]; then
+			echo "Error parsing power draw limits from Nvidia GPU$i!"
+			cleanup 1
+		elif [[ POWER[$i] -lt 1 ]]; then
+			echo "Power limit is lower than 1%, setting to 1%"
+			POWER[i]=1
+		fi
+		WPDRAW=$(($(($((PDRAW * 10)) * POWER[i])) / 1000))
+		if [[ $WPDRAW -gt $MAPDRAW ]]; then
+			echo "The Nvidia driver allows a maximum of $MAPDRAW watts for GPU$i. Setting to maximum."
+			WPDRAW=$MAPDRAW
+		elif [[ $WPDRAW -lt $MIPDRAW ]]; then
+			echo "The Nvidia driver allows a minimum of $MIPDRAW watts for GPU$i. Setting to minimum."
+			WPDRAW=$MIPDRAW
+		fi
+		echo "Setting power limit of GPU$i to ${POWER[i]}% (${WPDRAW} watts). (Requires root)"
+		sudo nvidia-smi --id="$GPUID" -pl "$WPDRAW" 1> /dev/null
+	done
 fi
 
 if [[ $POWERMIZER ]]; then
-    echo "Setting GPU to highest P-State."
-    nvidia-settings --assign [gpu:$GPUID]/GPUPowerMizerMode=0 1> /dev/null
+    for ((i=0; i<$GPUQTY; i++))
+	do
+		GPUID=$i
+		echo "Setting GPU$i to highest P-State."
+		nvidia-settings --assign [gpu:$GPUID]/GPUPowerMizerMode=0 1> /dev/null
+	done
 fi
 
 if [[ $GCLOCK ]]; then
-    echo "Setting GPU clock offset to $GCLOCK."
-    nvidia-settings --assign [gpu:$GPUID]/GPUGraphicsClockOffset["$PSTATE"]="$GCLOCK" 1> /dev/null
+    for ((i=0; i<$GPUQTY; i++))
+	do
+		GPUID=$i
+		echo "Setting GPU$i clock offset to ${GCLOCK[i]}."
+		nvidia-settings --assign [gpu:$GPUID]/GPUGraphicsClockOffset["$PSTATE"]="${GCLOCK[i]}" 1> /dev/null
+	done
+fi
+
+if [[ $LGCLOCK ]]; then
+    for ((i=0; i<$GPUQTY; i++))
+	do
+		GPUID=$i
+		echo "Setting GPU$i clock to ${LGCLOCK[i]}. (Requires root)"
+		sudo nvidia-smi --id="$GPUID" -lgc "${LGCLOCK[i]}" 1> /dev/null
+	done
 fi
 
 if [[ $MCLOCK ]]; then
-    echo "Setting GPU memory offset to $MCLOCK."
-    nvidia-settings --assign [gpu:$GPUID]/GPUMemoryTransferRateOffset["$PSTATE"]="$MCLOCK" 1> /dev/null
+    for ((i=0; i<$GPUQTY; i++))
+	do
+		GPUID=$i
+		echo "Setting GPU$i memory offset to ${MCLOCK[i]}."
+		nvidia-settings --assign [gpu:$GPUID]/GPUMemoryTransferRateOffset["$PSTATE"]="${MCLOCK[i]}" 1> /dev/null
+	done
 fi
 
 if [[ $LEDPERCENT ]] && [[ $LEDPERCENT -ge 0 ]] && [[ $LEDPERCENT -le 100 ]]; then
-    echo "Setting LED brightness to $LEDPERCENT percent."
-    nvidia-settings --assign [gpu:$GPUID]/GPULogoBrightness="$LEDPERCENT" 1> /dev/null
+    for ((i=0; i<$GPUQTY; i++))
+	do
+		GPUID=$i
+		echo "Setting GPU$i LED brightness to $LEDPERCENT percent."
+		nvidia-settings --assign [gpu:$GPUID]/GPULogoBrightness="$LEDPERCENT" 1> /dev/null
+	done
 fi
 
 if [[ $INTERVAL ]]; then
     CHANGEDFS=1
-    while true; do
-        CTEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader --id="$GPUID")
-        if [[ $CTEMP -lt ${TEMP[0]} ]]; then
-            CSPEED=$MINSPEED
-        elif [[ $CTEMP -ge ${TEMP[3]} ]]; then
-            CSPEED=${SPEED[3]}
-        elif [[ -n ${PAIRS[$CTEMP]} ]]; then
-            CSPEED=${PAIRS[$CTEMP]}
-        else
-            CSPEED=$SAFESPEED
-        fi
-        if [[ $SHOWCURRENT ]]; then
-            echo -ne "\033[2KCurrent Temp: $CTEMP Speed: $CSPEED\r"
-        fi
-        nvidia-settings --assign [gpu:$GPUID]/GPUFanControlState=1 --assign [fan:$GPUID]/GPUTargetFanSpeed=$CSPEED 1> /dev/null
-        sleep "$INTERVAL"
-    done
-    if [[ $SHOWCURRENT ]]; then
-        echo
-    fi
+	while true; do
+		for ((i=0; i<$GPUQTY; i++))
+		do
+			GPUID=$i
+			CTEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader --id="$GPUID")
+			if [[ $CTEMP -lt ${TEMP[0]} ]]; then
+				CSPEED=$MINSPEED
+			elif [[ $CTEMP -ge ${TEMP[3]} ]]; then
+				CSPEED=${SPEED[3]}
+			elif [[ -n ${PAIRS[$CTEMP]} ]]; then
+				CSPEED=${PAIRS[$CTEMP]}
+			else
+				CSPEED=$SAFESPEED
+			fi
+			if [[ $SHOWCURRENT ]]; then
+				#echo -ne "\033[2KCurrent GPU$i Temp: $CTEMP Speed: $CSPEED\r"
+				echo -ne "Current GPU$i Temp: $CTEMP Speed: $CSPEED\n"
+			fi
+			nvidia-settings --assign [gpu:$GPUID]/GPUFanControlState=1 --assign [fan:$((GPUID*FANOFFS))]/GPUTargetFanSpeed=$CSPEED 1> /dev/null
+		done
+		sleep "$INTERVAL"
+		if [[ $SHOWCURRENT ]]; then
+			echo
+		fi
+	done
 fi
